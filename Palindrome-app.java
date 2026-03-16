d73410fde4a8409ecb503d60dc1982cb023de859
RamKarthik-20 <rkn.20307@gmail.com>
2026-03-16 14:53:29 +0530

    UC2 commit

--- Palindrome-app.java
+++ Palindrome-app.java
@@ -1,22 +1,13 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        // Potential logic for Use Case 3: User Input
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string to check: ");
-        String input = scanner.nextLine();
+        // Step 10: Initial logic for Palindrome checking
+        String word = "madam";
+        String reversedWord = new StringBuilder(word).reverse().toString();
 
-        if (checkPalindrome(input)) {
-            System.out.println("The string is a palindrome.");
+        if (word.equals(reversedWord)) {
+            System.out.println(word + " is a palindrome.");
         } else {
-            System.out.println("The string is not a palindrome.");
+            System.out.println(word + " is not a palindrome.");
         }
-    }
-
-    public static boolean checkPalindrome(String str) {
-        String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
-        String reversed = new StringBuilder(cleanStr).reverse().toString();
-        return cleanStr.equals(reversed);
     }
 }
